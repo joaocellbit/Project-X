6a4cf4e38d580285d56ca6a8b6a8ad842b994c9b
joaocellbit <joaomarciopradosilva@gmail.com>
2025-12-09 19:58:48 -0300

    Remove unused image and update server logic
    Deleted the unused 'BaseWhiteMale.png' and its import file. Updated 'Saiyajin.tscn' to remove the 'Sexo' property. Enhanced 'Server.gd' to add new dictionaries for player, planet, and NPC management, and implemented client update logic for player IDs.

--- Server Script/Server.gd
+++ Server Script/Server.gd
@@ -1,8 +1,11 @@
 extends Node
-
-@onready var playerId
+@onready var playerid:Dictionary # player_id -> nome
+@onready var player_state:Dictionary # player_id -> body,clothes,hair etc
+@onready var planetid:Dictionary # planet_id -> nome
 @onready var player_planet:Dictionary  # player_id -> planet_id
 @onready var Planet_player:Dictionary  # planet_id -> Array[player_id]
+@onready var npc_planet:Dictionary # npc_id -> planet_id
+@onready var planet_npc:Dictionary # planet_id -> array[Npc_id]
 @onready var Server = ENetMultiplayerPeer.new()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,6 +23,8 @@ func criar_server(Porta:int) -> void:
 		return
 	multiplayer.multiplayer_peer = Server
 	print("ok")
+	playerid[1] = "server"
+	print(playerid)
 	multiplayer.peer_connected.connect(New_connection)
 
 
@@ -31,3 +36,11 @@ func criar_cliente(ip:String, Porta:int) -> void:
 
 func New_connection(id) -> void:
 	print(id)
+	playerid[id] = "test"
+	print(playerid,  multiplayer.get_unique_id())
+	rpc("update_client", playerid)
+
+@rpc("authority","call_remote","reliable")
+func update_client(ids:Dictionary):
+	playerid = ids
+	print(playerid, multiplayer.get_unique_id())
